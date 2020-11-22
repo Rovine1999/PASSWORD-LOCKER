adab0ec7fdb0f408dd072ebc9b0bba9d10343e4e
Rovine1999 <rovinewanjala99@gmail.com>
2020-11-22 04:36:51 +0300

    modified

--- Credential.py
+++ Credential.py
@@ -1,110 +1,109 @@
 #!/usr/bin/env python3.9
-from contact import Contact
+from account import Account
 
 
-def create_contact(fname,lname,phone,email):
-    '''
-    Function to create a new contact
-    '''
-    new_contact = Contact(fname,lname,phone,email)
-    return new_contact
+    def create_account(account_name,username,password):
+        '''
+        Function to create a new account
+        '''
+        new_account = Account(account_name,username,password)
+        return new_account
 
-def save_contacts(contact):
-    '''
-    Function to save contact
-    '''
-    contact.save_contact()
+    def save_accounts(account):
+        '''
+        Function to save account
+        '''
+        account.save_account()
 
-def del_contact(contact):
-    '''
-    Function to delete a contact
-    '''
-    contact.delete_contact()
-
-
-def find_contact(number):
-    '''
-    Function that finds a contact by number and returns the contact
-    '''
-    return Contact.find_by_number(number)
-
-def check_existing_contacts(number):
-    '''
-    Function that check if a contact exists with that number and return a Boolean
-    '''
-    return Contact.contact_exist(number)
-
-def display_contacts():
-    '''
-    Function that returns all the saved contacts
-    '''
-    return Contact.display_contacts()
+    def del_account(account):
+        '''
+        Function to delete an account
+        '''
+        account.delete_account()
 
 
+    def find_account(account_name):
+        '''
+        Function that finds an account by account_name and returns the account
+        '''
+        return Account.find_by_account(account_name)
 
-def main():
-            print("Hello Welcome to your contact list. What is your name?")
-            user_name = input()
+    def check_existing_accounts(account_name):
+        '''
+        Function that check if an account exists with that account_name and return a Boolean
+        '''
+        return Account.account_exist(account_name)
 
-            print(f"Hello {user_name}. what would you like to do?")
+    def display_accounts():
+         '''
+        Function that returns all the saved contacts
+        '''
+        return Account.display_accounts()
+
+
+
+    def main():
+            print("Hello Welcome to your account list. What is the name of your account?")
+            account_name = input()
+
+            print(f"Hello {account_name}. what would you like to do?")
             print('\n')
 
             while True:
-                    print("Use these short codes : cc - create a new contact, dc - display contacts, fc -find a contact, ex -exit the contact list ")
+                    print("Use these short codes : cc - create a new account, dc - display accounts, fc -find an account, ex -exit the account list ")
 
                     short_code = input().lower()
 
                     if short_code == 'cc':
-                            print("New Contact")
+                            print("New Account")
                             print("-"*10)
 
-                            print ("First name ....")
-                            f_name = input()
+                            print ("Account name")
+                            account_name = input()
 
-                            print("Last name ...")
-                            l_name = input()
+                            print("Username ...")
+                            username= input()
 
-                            print("Phone number ...")
-                            p_number = input()
+                            print("Password ...")
+                            password = input()
 
-                            print("Email address ...")
-                            e_address = input()
+                            
 
 
-                            save_contacts(create_contact(f_name,l_name,p_number,e_address)) # create and save new contact.
+                            save_accounts(create_account(account_name.username,password)) # create and save new contact.
                             print ('\n')
-                            print(f"New Contact {f_name} {l_name} created")
+                            print(f"New Account {account_name} {username} created")
                             print ('\n')
 
                     elif short_code == 'dc':
 
-                            if display_contacts():
-                                print("Here is a list of all your contacts")
+                            if display_accounts():
+                                print("Here is a list of all your accounts")
                                 print('\n')
 
-                            for contact in display_contacts():
-                                print(f"{contact.first_name} {contact.last_name} .....{contact.phone_number}")
+                            for account in display_accounts():
+                                print(f"{account_name} {username} .....{password}")
 
                                 print('\n')
                             else:
                                 print('\n')
-                                print("You dont seem to have any contacts saved yet")
+                                print("You dont seem to have any accounts saved yet")
                                 print('\n')
 
                     elif short_code == 'fc':
 
-                            print("Enter the number you want to search for")
+                            print("Enter the account_name you want to search for")
 
-                            search_number = input()
-                            if check_existing_contacts(search_number):
-                                    search_contact = find_contact(search_number)
-                                    print(f"{search_contact.first_name} {search_contact.last_name}")
+                            search_account_name = input()
+                            if check_existing_accounts(search_account_name):
+                                    search_contact = find_account(search_account_name)
+                                    print(f"{search_account_name} ")
                                     print('-' * 20)
 
-                                    print(f"Phone number.......{search_contact.phone_number}")
-                                    print(f"Email address.......{search_contact.email}")
+                                    print(f"Account_name.......{search_account_name}")
+                                    # print(f"Email address.......{search_contact.email}")
                             else:
-                                    print("That contact does not exist")
+                                    print("That account does not exist")
 
                     elif short_code == "ex":
                             print("Bye .......")
--- account_test.py
+++ account_test.py
@@ -31,98 +31,98 @@ class TestAccount(unittest.TestCase):
        
 
 
-    def test_save_contact(self):
+    def test_save_account(self):
         '''
-        test_save_contact test case to test if the contact object is saved into
-         the contact list
+        test_save_account test case to test if the account object is saved into
+         the account list
         '''
-        self.new_contact.save_contact() # saving the new contact
-        self.assertEqual(len(Contact.contact_list),1)
+        self.new_account.save_account() 
+        self.assertEqual(len(Account.account_list),1)
 
-        # setup and class creation up here
+        
     def tearDown(self):
             '''
             tearDown method that does clean up after each test case has run.
             '''
-            Contact.contact_list = []
+            Account.account_list = []
 
         # other test cases here
-    def test_save_multiple_contact(self):
+    def test_save_multiple_account(self):
             '''
-            test_save_multiple_contact to check if we can save multiple contact
-            objects to our contact_list
+            test_save_multiple_account to check if we can save multiple account
+            objects to our account_list
             '''
-            self.new_contact.save_contact()
-            test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
+            self.new_account.save_account()
+            test_contact = Contact("Test","username","account_name","password") 
             test_contact.save_contact()
-            self.assertEqual(len(Contact.contact_list),2)
+            self.assertEqual(len(Account.account_list),2)
 
-        # Items up here...
-    def test_save_multiple_contact(self):
+        
+    def test_save_multiple_account(self):
             '''
-            test_save_multiple_contact to check if we can save multiple contact
-            objects to our contact_list
+            test_save_multiple_account to check if we can save multiple account
+            objects to our account_list
             '''
-            self.new_contact.save_contact()
-            test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
+            self.new_account.save_account()
+            test_account = Account("Test","username","account_name","password") 
             test_contact.save_contact()
-            self.assertEqual(len(Contact.contact_list),2)
+            self.assertEqual(len(Account.account_list,2)
 
             # More tests above
-    def test_delete_contact(self):
+    def test_delete_account(self):
             '''
-            test_delete_contact to test if we can remove a contact from our contact list
+            test_delete_account to test if we can remove an account from our account list
             '''
-            self.new_contact.save_contact()
-            test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
-            test_contact.save_contact()
+            self.new_account.save.account()
+            test_account = Account("Test","username","account_name","password") # new contact
+            test_account.save_account()
 
-            self.new_contact.delete_contact()# Deleting a contact object
-            self.assertEqual(len(Contact.contact_list),1)
+            self.new_contact.test_delete_account()# Deleting a contact object
+            self.assertEqual(len(Account.account_list),1)
 
 
 
-    def test_find_contact_by_number(self):
+    def test_find_account_by_account_name(self):
             '''
-            test to check if we can find a contact by phone number and display information
+            test to check if we can find an account by account_name and display information
             '''
 
-            self.new_contact.save_contact()
-            test_contact = Contact("Test","user","0711223344","test@user.com") # new contact
-            test_contact.save_contact()
+            self.new_account.save_account()
+            test_account = Account("Test","username","account_name","password") # new contact
+            test_account.save_account()
 
-            found_contact = Contact.find_by_number("0711223344")
+            found_account = Account.find_by_account_name("facebook")
 
-            self.assertEqual(found_contact.email,test_contact.email)
+            # self.assertEqual(found_contact.email,test_contact.email)
 
 
 
 
    
 
-    def test_contact_exists(self):
+    def test_account_exists(self):
         '''
-        test to check if we can return a Boolean  if we cannot find the contact.
+        test to check if we can return a Boolean  if we cannot find the account.
         '''
 
-        self.new_contact.save_contact()
-        test_contact = Contact("Test","user","0711223344","test@user.com") # new contact
-        test_contact.save_contact()
+        self.new_account.save_account()
+        test_account = Account("Test","username","account_name","password") # new contact
+        test_account.save_account()
 
-        contact_exists = Contact.contact_exist("0711223344")
+        account_exists = Account.account_exist("account_name")
 
-        self.assertTrue(contact_exists)
+        self.assertTrue(account_exists)
 
 
     
 
 
-    def test_display_all_contacts(self):
+    def test_display_all_accounts(self):
         '''
         method that returns a list of all contacts saved
         '''
 
-        self.assertEqual(Contact.display_contacts(),Contact.contact_list)
+        self.assertEqual(Contact.display_account(),Account.account_list)
 
 
 
